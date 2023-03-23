052da43163bbfe03974521a4aca6c4d9363b384e
olivia <oliviabir19@gmail.com>
2023-03-23 11:12:32 -0700

    initial setup for seed files

---
+++ backend/db/seeders/20230323180755-demo-review.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'Reviews'
+    return queryInterface.bulkInsert(options, [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Reviews'
+    await queryInterface.bulkDelete(options, null, {});
+  }
+};

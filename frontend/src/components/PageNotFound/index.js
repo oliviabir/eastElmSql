5b99bd2bacc51b8e09e487d1b23aac8d1dc20877
olivia <oliviabir19@gmail.com>
2023-04-12 19:11:59 -0700

    404

---
+++ frontend/src/components/PageNotFound/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PageNotFound.css'
+
+const PageNotFound = () => {
+  return (
+    <h1 className='page-not-found'>Sorry, we couldn't find the page you're looking for</h1>
+  )
+}
+
+export default PageNotFound
